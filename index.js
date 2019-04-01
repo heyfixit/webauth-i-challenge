03419e5b1f9d32970870c2428e3f66eb37437f23
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-01 15:22:46 -0400

    refactor index, include sqlite

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const authRouter = require('./auth/controller');
-// const usersRouter = require('./users/controller');
+const restricted = require('./auth/restricted');
+
+const authController = require('./auth/controller');
+const usersController = require('./users/controller');
 const server = express();
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-// server.use('/api', authRouter);
-// server.use('/api/users', usersRouter);
-server.use('/', (req, res) => res.send('API up and running'));
+server.use('/api/restricted/*', restricted);
+server.use('/api', authController);
+server.use('/api/restricted/users', usersController);
+server.use('/', (_req, res) => res.send('API up and running'));
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`\n*** API running on http://localhost:${port} ***\n`)
