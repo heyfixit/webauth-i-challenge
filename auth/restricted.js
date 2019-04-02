50336dcb95e28caf1acfae880f8200d406615269
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-02 14:16:05 -0400

    modify restricted middleware, check session

--- auth/restricted.js
+++ auth/restricted.js
@@ -1,21 +1,7 @@
-const bcrypt = require('bcrypt');
-const Users = require('../users/model');
-
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.getByUsername(username)
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Unauthorized' });
-        }
-      })
-      .catch(error => res.status(500).json(error));
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(401).json({ message: 'Unauthorized user' });
+    res.status(401).json({ message: 'Unauthorized' });
   }
 };
